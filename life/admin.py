af93c250842f56113f06a187b935acb13445bce2
Oghenekevbe Egume <oghenekevbe.egume@gmail.com>
2023-04-25 22:10:21 +0100

    added models

--- life/admin.py
+++ life/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Doctor)
+admin.site.register(Notes)
+admin.site.register(Patient)
+admin.site.register(Post)
+admin.site.register(Comment)
--- life/models.py
+++ life/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Doctor(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name.username
+
+class Patient(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    doctor_notes = models.ManyToManyField(Doctor, through='Notes')
+
+    def __str__(self):
+        return self.name.username
+
+class Notes(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    notes = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.doctor} | {self.patient}"
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=225)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.subject} | {self.author}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.subject} | {self.author}"
