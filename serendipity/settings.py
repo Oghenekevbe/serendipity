e581fbbb8e94188084d950fe079505455e069bb2
Oghenekevbe <111989350+Oghenekevbe@users.noreply.github.com>
2023-06-22 04:25:26 -0700

    Update settings.py

--- serendipity/settings.py
+++ serendipity/settings.py
@@ -84,21 +84,21 @@ WSGI_APPLICATION = 'serendipity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'serenlife',
-        'MONGO_URI' : config('MONGO_URI')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'serenlife',
+#         'MONGO_URI' : config('MONGO_URI')
+#     }
+# }
 
 
 
